3ff858fcc3c384c14be64dc8f65cbf9bf3fc0216
NeerajSirvisetti <32873782+NeerajSirvisetti@users.noreply.github.com>
2020-05-05 20:02:04 +0530

    Delete 4-Number complement.py

--- week1/4-Number complement.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findComplement(self, num: int) -> int:
-        b= (bin(num)[2:] )
-        b=b.replace("1","4")
-        b=b.replace("0","1")
-        b=b.replace("4","0")
-        return int(b,2)
